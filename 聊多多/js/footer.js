7f856d27cc4a8d8f1561cd33b0ab3b65916634c4
anty-song <18233582827@163.com>
2018-05-29 12:02:20 +0800

    删除重复租朋友文件夹修改聊多多

--- 聊多多/js/footer.js
+++ 聊多多/js/footer.js
@@ -2,6 +2,6 @@ document.writeln("    <a class='kefu' href='#'>");
 document.writeln("    	<img src='images/kefu.png' alt='聊多多'>");
 document.writeln("    </a>");
 document.writeln("    <footer>");
-document.writeln("      <p class='saying'>你会想我的对吧</p>");
+document.writeln("      <p class='saying hidden-xs'>你会想我的对吧</p>");
 document.writeln("      <p class='address'>(c)2016-2025 liaoduoduo18 All Rights Reserved 北京市昌平区回龙观镇科星西路106号院6号楼</p>");
 document.writeln("    </footer>");
